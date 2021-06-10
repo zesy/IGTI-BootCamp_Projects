201a23fff9edff42b0338ac3cc014d32de96684a
ZeSy <zesy.wsa@gmail.com>
2021-06-09 23:00:59 -0300

    Refatored some var names

--- 03_ReactJS-Investiments/src/components/Investment.jsx
+++ 03_ReactJS-Investiments/src/components/Investment.jsx
@@ -2,8 +2,8 @@ import Report from "./Report";
 
 export default function Investment({descr, children: reports}) {
 
-    function calcPercent(currentValue, afterValue){
-        return ((currentValue - afterValue) * 100) / afterValue;
+    function calcPercent(currentValue, beforeValue){
+        return ((currentValue - beforeValue) * 100) / beforeValue;
     }
     
     const ordenedReports = reports.sort((a, b) => {
@@ -12,21 +12,21 @@ export default function Investment({descr, children: reports}) {
         return 0;
     });
 
-    const newOrdenedReports = ordenedReports.map((or, index) => {
+    const newOrdenedReports = ordenedReports.map((ordReport, index) => {
         if(index === 0)
-            return {...or, percent: 0}
+            return {...ordReport, percent: 0}
         else{
-            let afterValue = parseFloat(ordenedReports[index-1].value)
-            let percent = calcPercent(parseFloat(or.value), afterValue);
-            return {...or, percent: percent}   
+            let beforeValue = parseFloat(ordenedReports[index-1].value)
+            let percent = calcPercent(parseFloat(ordReport.value), beforeValue);
+            return {...ordReport, percent: percent}   
         }
     });
 
     const firstReport = parseFloat(newOrdenedReports[0].value);
     const lastReport = parseFloat(newOrdenedReports[(newOrdenedReports.length)-1].value);
     const totalRent = (lastReport - firstReport).toFixed(2);
-    const totalPercent = calcPercent(lastReport, firstReport).toFixed(2);
-    const strTotalPercent = totalPercent > 0 ? ("+"+totalPercent) : (totalPercent);
+    const totalRentPercent = calcPercent(lastReport, firstReport).toFixed(2);
+    const strTotalPercent = totalRentPercent > 0 ? ("+"+totalRentPercent) : (totalRentPercent);
 
     const totalRentBgColor = totalRent > 0 ? "text-green-500" : totalRent < 0 ? "text-red-500" : "";
 
@@ -34,8 +34,8 @@ export default function Investment({descr, children: reports}) {
         <div className="border border-gray-400 w-4/5 sm:w-4/5 md:w-4/5 lg:w-3/5 mx-auto my-3 p-2">
             <h1 className="text-lg font-semibold text-center">{descr}</h1>
             <p className="text-sm text-center mb-5 font-semibold">Rendimento Total: <span className={totalRentBgColor}>R$ {totalRent} ({strTotalPercent}%)</span></p>
-            {newOrdenedReports.map(rep => {
-                return <Report key={rep.id}>{rep}</Report>
+            {newOrdenedReports.map(report => {
+                return <Report key={report.id}>{report}</Report>
             })}
         </div>
     )
--- 03_ReactJS-Investiments/src/components/Report.jsx
+++ 03_ReactJS-Investiments/src/components/Report.jsx
@@ -6,13 +6,14 @@ export default function Report({children: report}) {
     const percent = parseFloat(report.percent).toFixed(2);
     const strPercent = percent > 0 ? "+"+percent : percent;
 
-    const bgColor = (percent > 0 ? "text-green-400" : percent < 0 ? "text-red-400" : "");
+    const txtColor = (percent > 0 ? "text-green-400" : percent < 0 ? "text-red-400" : "");
     const months = ["jan", "fev", "mar", "abr", "mai", "jun", "jul", "ago", "set", "out", "nov", "dez"];
+    
     return (
         <div className="flex flex-row border-b border-gray-300 items-center">
             <p className="mr-5 text-sm font-mono">{months[month-1]}/{year}</p>
-            <p className={bgColor}>R$ {value}</p>
-            <p className={"ml-auto text-sm font-semibold "+bgColor}>{strPercent}%</p>
+            <p className={txtColor}>R$ {value}</p>
+            <p className={"ml-auto text-sm font-semibold "+txtColor}>{strPercent}%</p>
         </div>
     )
 }
